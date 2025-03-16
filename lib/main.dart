d154e9620dfff4a5b4751b2e9effbef0fe039790
Farojeh <areejmh2003@gmail.com>
2025-03-16 15:18:27 +0300

    DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:advanced_project/Auth/model/user_model.dart';
 import 'package:advanced_project/utils/app_router.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,7 +10,11 @@ UserModel? mainuser;
 void main() async {
    WidgetsFlutterBinding.ensureInitialized();
    userInfo = await SharedPreferences.getInstance();
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
 }
 
 class MyApp extends StatelessWidget {
